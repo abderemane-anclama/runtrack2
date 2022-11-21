63017309c0ea0bf91a33303e5dff71345791a427
abderemane-anclama <abderemane.anclam@laplateforme.io>
2022-11-21 09:01:23 +0100

    ajour du projet jour02

---
+++ jour02/job01/index.php
@@ -0,0 +1,14 @@
+<?php 
+echo "liste de 1337 nombres entier naturel";
+for($i=1 ; $i<=1337 ; $i++){    
+    
+    if($i==42){
+        echo "<b>$i</b><br>";
+    } 
+    else{
+        echo "$i  <br>";
+    }
+
+    
+}
+?>
